c41cdcc0a8a3cfa19f27a9c87f36291cdaf66e82
Breeze11 <485220461@qq.com>
2018-09-06 23:35:23 +0800

    使用拦截器完成登录拦截，使用拦截器记录请求响应日志

---
+++ springBootDemo8/src/main/java/com/djs/LoggerConfiguration.java
@@ -0,0 +1,17 @@
+package com.djs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.djs.interceptor.LoggerInterceptor;
+
+@Configuration
+public class LoggerConfiguration extends WebMvcConfigurerAdapter{
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LoggerInterceptor()).addPathPatterns("/**");
+	}
+	
+}
